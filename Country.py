d63bc9969270554d3c03cd49cb4ae4b1e1f70766
Georgecw <51824647+Georgecw@users.noreply.github.com>
2019-06-14 21:37:05 +0800

    Add files via upload

--- Country.py
+++ Country.py
@@ -13,18 +13,26 @@ class Country():
         #每年增加金币
         self.gold += 10
 
-    def energy(self):
+    def deal_energy(self):
         #处理能源带来的各属性变化
         self.power = Energy()
         self.power.build_energy()
         for key,value in self.locks.items():
-            kay = key - "_lock"
-            if power.built_energy[kay] > value:
-                power.built_energy[kay] = value
-        for key , value in power.built_energy.item():
-            self.gold -= self.power.e_list[key][0]*value
-            self.energy += self.power.e_list[key][1]*value
-            self.pollution += self.power.e_list[key][2]*value
+            kay = key.strip("_lock")
+            try:
+                if self.power.built_list[kay] > value:
+                    self.power.built_list[kay] = value
+            except KeyError:
+                self.power.built_list[kay] = 0
+        for key , value in self.power.built_list.items():
+            if self.power.e_list[key][3] == 1:
+                self.gold -= self.power.e_list[key][0]*value
+                self.energy += self.power.e_list[key][1]*value
+                self.pollution += self.power.e_list[key][2]*value
+
+A = Country(1,6,6)
+A.deal_energy()
+print(A.gold)
             
         
         
