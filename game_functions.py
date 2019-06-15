128ba77222e0095a66d96a83527b3f9dbadb3b9d
Georgecw <51824647+Georgecw@users.noreply.github.com>
2019-06-15 22:21:06 +0800

    fixed a extremely terrible keyerror which can cause the whole game crash

--- game_functions.py
+++ game_functions.py
@@ -4,7 +4,7 @@ def country_init_1(x):
     init_input = "请输入" + str(x) + "国的"
     nuclear_lock = input("\n"+ init_input + "核能上限: ")
     ice_lock = input("\n"+ init_input + "可燃冰上限: ")
-    creature_lock = input("\n"+ init_input + "生物质能上限: ")
+    eature_lock = input("\n"+ init_input + "生物质能上限: ")
     oil_lock = input("\n" + init_input + "石油上限: ")
     coal_lock = input("\n"+ init_input + "煤上限: ")
     x = Country(nuclear_lock,ice_lock,creature_lock,oil_lock,coal_lock)
