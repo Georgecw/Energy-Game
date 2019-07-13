b4704582d12bf763d4415e9d1aa9cdaa4d2f3f85
Frigus27 <Frigus27@outlook.com>
2019-07-13 22:39:50 +0800

    Improved code quality

--- game_functions.py
+++ game_functions.py
@@ -1,12 +1,12 @@
 from Country import Country
-from punishment import *
-from target import *
+from punishment import random_punishment
+from target import random_target
 
 def country_init(x):
     init_input = "请输入" + str(x) + "国的"
     nuclear_lock = input("\n"+ init_input + "核能上限: ")
     ice_lock = input("\n"+ init_input + "可燃冰上限: ")
-    eature_lock = input("\n"+ init_input + "生物质能上限: ")
+    creature_lock = input("\n"+ init_input + "生物质能上限: ")
     oil_lock = input("\n" + init_input + "石油上限: ")
     coal_lock = input("\n"+ init_input + "煤上限: ")
     x = Country(nuclear_lock,ice_lock,creature_lock,oil_lock,coal_lock)
