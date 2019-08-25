d512294dcb72e1ac6bc61ad0f8b9e3e7fe2ca052
Frigus27 <Frigus27@outlook.com>
2019-08-25 22:37:19 +0800

    fixed another bug lol

--- Start_page.py
+++ Start_page.py
@@ -47,6 +47,6 @@ def start():
                 if start_msgs[-1].msg_rect.collidepoint(mouse_x,mouse_y):
                     pygame.quit()
                     
-if __name__ == if __name__ == "__main__":
+if __name__ == "__main__":
     start()
 
---
+++ UI/Main.py
@@ -0,0 +1,9 @@
+import pygame
+import game_functions as gf
+from Msg import Msg
+from Settings import Settings
+
+def main():
+    pygame.init()
+
+    pygame.quit()
